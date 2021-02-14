fefcc43451169c7625fe50644644384576073f05
Bluefire123 <17679976+Bluefire123@users.noreply.github.com>
2021-02-14 09:18:57 -0800

    👋start

---
+++ src/main/java/bluefirephoenix/brokennametags/Brokennametags.java
@@ -0,0 +1,10 @@
+package bluefirephoenix.brokennametags;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Brokennametags implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
